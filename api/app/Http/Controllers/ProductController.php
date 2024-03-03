b45564bb045e242278fd15f1ebd0cd9d11fa5248
Roger Cruz <92161038+RogerGCruz@users.noreply.github.com>
2024-03-02 22:50:17 -0300

    docs: :coffin: Removed code not used e description injection dependency on construct

--- api/app/Http/Controllers/ProductController.php
+++ api/app/Http/Controllers/ProductController.php
@@ -2,15 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Product;
 use App\Repositories\ProductRepositoryInterface;
-use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     private ProductRepositoryInterface $productRepository;    
     public function __construct(ProductRepositoryInterface $productRepository)
     {
+        // Dependency injection
         $this->productRepository = $productRepository;
     }
 
